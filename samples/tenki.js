fb1816285ad2336b4e0c914084264772f2b673f1
SHIROYAMA Takayuki <puresnow@mac.com>
2026-01-21 18:06:47 +0900

    ADD: show all sub areas. ADD: update title.

--- samples/tenki.js
+++ samples/tenki.js
@@ -31,14 +31,6 @@ class AreaCodes {
         throw new Error("no office area");
     }
 
-//    get_fistarea( areacode ) {
-//        if( this.area.data.offices[areacode] ) {
-//            return this.area_data.offices[areacode].children[0] ;
-//        }else{
-//            return "";
-//        }
-//    }
-
     async get_arealist() {
         if( Object.keys( this.area_list.length <= 0 ) ) {
    
@@ -63,12 +55,43 @@ async function get_weather_data( areacode ) {
 
     const weather_data = await response.json() ;
 
-    let retdict = {} ;
-    return [ weather_data[0].timeSeries[0].areas[0].weathers[0] ,
-             weather_data[0].timeSeries[0].areas[0].weatherCodes[0] ];
+    let retarray = [] ;
 
-    return retdict
+    for( d in weather_data[0].timeSeries[0].areas ) {
+        // console.log(  weather_data[0].timeSeries[0].areas[d] );
+
+        retarray.push( { "name" :       weather_data[0].timeSeries[0].areas[d].area.name , 
+                         "weather" :    weather_data[0].timeSeries[0].areas[d].weathers[0],
+                         "weatherCode" : weather_data[0].timeSeries[0].areas[d].weatherCodes[0] } );
+    }
+
+    return retarray;
 }
+
+
+// mapping from weather codes to real svg numbers.
+const wmap = { "100":"500", "101":"501", "102":"502", "103":"502", "104":"504",
+               "105":"504", "106":"502", "107":"502", "108":"502", "110":"510",
+               "111":"510", "112":"512", "113":"512", "114":"512", "115":"515",
+               "116":"515", "117":"515", "118":"512", "119":"512", "120":"502",
+               "121":"502", "122":"512", "123":"500", "124":"500", "125":"512",
+               "126":"512", "127":"512", "128":"512", "130":"500", "131":"500",
+               "132":"501", "140":"502", "160":"504", "170":"504", "181":"515",
+               "201":"601", "203":"202", "205":"204", "206":"202", "207":"202",
+               "208":"202", "209":"200", "210":"610", "211":"610", "213":"212",
+               "214":"212", "216":"215", "217":"215", "218":"212", "219":"212",
+               "220":"202", "221":"202", "222":"212", "223":"601", "224":"212",
+               "225":"212", "226":"212", "228":"215", "229":"215", "230":"215",
+               "231":"200", "240":"202", "250":"204", "260":"204", "270":"204",
+               "281":"215", "301":"701", "304":"300", "306":"300", "309":"303",
+               "311":"711", "315":"314", "316":"711", "317":"313", "320":"711",
+               "321":"313", "322":"303", "323":"711", "324":"711", "325":"711",
+               "326":"314", "327":"314", "328":"300", "329":"300", "340":"400",
+               "350":"300", "361":"811", "371":"413", "401":"801", "405":"400",
+               "407":"406", "409":"403", "411":"811", "420":"811", "421":"413",
+               "422":"414", "423":"414", "425":"400", "426":"400", "427":"400",
+               "450":"400" };
+
 
 async function main() {
 
@@ -83,14 +106,24 @@ async function main() {
 
     let areacodes = new AreaCodes();
     aret = await areacodes.get_areacode( areaname );
-    console.log( aret );
+    // console.log( aret );
 
     const wrets = await get_weather_data( aret );
-    console.log( wrets[0] ) ;
-    console.log( wrets[1] );
 
-    const wlist = await areacodes.get_arealist();
-    console.log( wlist );
+    for( w in wrets ) {
+        console.log( wrets[w].name ) ;
+        console.log( wrets[w].weather );
+        if( wmap[ wrets[w].weatherCode ] ) {
+            // console.log( "mapped " + wrets[w].weatherCode + " -> " + wmap[ wrets[w].weatherCode ] );
+
+            console.log( "https://www.jma.go.jp/bosai/forecast/img/" + wmap[ wrets[w].weatherCode ] + ".svg" );
+        }else{
+            console.log( "https://www.jma.go.jp/bosai/forecast/img/" + wrets[w].weatherCode  + ".svg" );
+        }
+    }
+    console.log("")
+    // const wlist = await areacodes.get_arealist();
+    // console.log( wlist );
 
 
 }
--- samples/tenki.py
+++ samples/tenki.py
@@ -7,6 +7,27 @@ AREA_URL="https://www.jma.go.jp/bosai/common/const/area.json"
 WEATHER_URL="https://www.jma.go.jp/bosai/forecast/data/forecast/%s.json"
 ICON_URL="https://www.jma.go.jp/bosai/forecast/img/%s.svg"
 
+wmap = { "100":"500", "101":"501", "102":"502", "103":"502", "104":"504",
+         "105":"504", "106":"502", "107":"502", "108":"502", "110":"510",
+         "111":"510", "112":"512", "113":"512", "114":"512", "115":"515",
+         "116":"515", "117":"515", "118":"512", "119":"512", "120":"502",
+         "121":"502", "122":"512", "123":"500", "124":"500", "125":"512",
+         "126":"512", "127":"512", "128":"512", "130":"500", "131":"500",
+         "132":"501", "140":"502", "160":"504", "170":"504", "181":"515",
+         "201":"601", "203":"202", "205":"204", "206":"202", "207":"202",
+         "208":"202", "209":"200", "210":"610", "211":"610", "213":"212",
+         "214":"212", "216":"215", "217":"215", "218":"212", "219":"212",
+         "220":"202", "221":"202", "222":"212", "223":"601", "224":"212",
+         "225":"212", "226":"212", "228":"215", "229":"215", "230":"215",
+         "231":"200", "240":"202", "250":"204", "260":"204", "270":"204",
+         "281":"215", "301":"701", "304":"300", "306":"300", "309":"303",
+         "311":"711", "315":"314", "316":"711", "317":"313", "320":"711",
+         "321":"313", "322":"303", "323":"711", "324":"711", "325":"711",
+         "326":"314", "327":"314", "328":"300", "329":"300", "340":"400",
+         "350":"300", "361":"811", "371":"413", "401":"801", "405":"400",
+         "407":"406", "409":"403", "411":"811", "420":"811", "421":"413",
+         "422":"414", "423":"414", "425":"400", "426":"400", "427":"400",
+         "450":"400" }
 
 def main():
     areaname = "tokyo"
@@ -36,7 +57,7 @@ def main():
             if not "enName" in area_data["offices"][k] :
                 skip
 
-            if area_data["offices"][k]["enName"].lower().startswith( areaname ):
+            if area_data["offices"][k]["enName"].lower().startswith( areaname.lower() ):
                 areacode = k
                 childcode = area_data["offices"][k]["children"][0]
                 break 
@@ -55,12 +76,16 @@ def main():
         if not "areas" in weather_data[0]["timeSeries"][0] :
             print("no area area in %s ( %s )." % ( areaname, areacode ),  file=sys.stderr )
             exit(1)
-
-        d = weather_data[0]["timeSeries"][0]["areas"][0]
-        print( "%s ( %s )" % ( d["area"]["name"], areacode ) )
-        print( "    %s" % d["weathers"][0] )
-        print( "    " + ICON_URL % d["weatherCodes"][0] )
-
+        ### print( weather_data[0]["timeSeries"][0]["areas"] )
+        for d in weather_data[0]["timeSeries"][0]["areas"] :
+            print( "%s ( %s )" % ( d["area"]["name"], d["area"]["code"]) )
+            print( "    %s" % d["weathers"][0] )
+            if d["weatherCodes"][0] in wmap :
+                ## print("mapped %s -> %s" % ( d["weatherCodes"][0] ,  wmap[ d["weatherCodes"][0] ] ) )
+                print( "    " + ICON_URL % wmap[ d["weatherCodes"][0] ] )
+            else:
+                print( "    " + ICON_URL % d["weatherCodes"][0] )
+        print("")
     else:
         print("no area data." , file=sys.stderr )
 
